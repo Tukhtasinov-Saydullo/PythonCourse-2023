f27f32d269ecd1c07eaa6f2c4a7573820ef3010d
Tukhtasinov-Saydullo <Tukhtasinov-Saydullo@github.com>
2023-01-27 12:04:59 +0500

    Update

--- Model3/task2/task4.py
+++ Model3/task2/task4.py
@@ -2,10 +2,22 @@ def only_even_parameters(func):
     def add_nums(a, b):
         if a % 2 == 0 and b % 2 == 0:
             return a + b
-        else:
-            print("Please add only even numbers!")
+        print("Please add only even numbers!")
 
     return add_nums
+
+
+def only_even_parameters2(func):
+    def math_mult(a, b, c, d):
+        if a and b and c and d % 2 == 0:
+            return a * b * c * d
+        print('Please add only even numbers!')
+
+    return math_mult
+
+@only_even_parameters2
+def mult(a, b, c, d):
+    return a * b * c * d
 
 
 @only_even_parameters
@@ -13,4 +25,6 @@ def add(a, b):
     return a + b
 
 
-print(add(2, 1))
+print(add(2, 2))
+
+print(mult(6, 4, 2, 2))
