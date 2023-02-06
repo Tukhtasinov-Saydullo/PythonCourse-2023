14007dd153b312cb78d4f72f288ead323fbdb523
Tukhtasinov Saydullo <Tukhtasinov.Saydullo@yahoo.com>
2023-02-06 13:12:59 +0500

    Update

--- TkinterProjects/LatinKyril/app.py
+++ TkinterProjects/LatinKyril/app.py
@@ -1,9 +1,9 @@
 from transliterate import to_cyrillic
-from tkinter import Tk, Label, Button, Radiobutton, Text, Entry, messagebox
+from tkinter import Tk, Label, Button, Radiobutton, Entry, messagebox
 
 # Window
 window = Tk()
-window.title('To Cyrillic')
+window.title('LatinCyrillic App')
 window.geometry('480x240')
 
 # Input
@@ -12,18 +12,32 @@ label_input.grid(row=1, column=0)
 label_entry = Entry(window)
 label_entry.grid(row=1, column=1)
 
-
-# Command
-def to_cyrillic_app():
+cyrill = 'To Cyrillic'
+latin = 'To Latin'
+# Commands Func
+def to_cyrillic_func():
     text = label_entry.get()
-    result_ = to_cyrillic(text)
-    result.config(text=result_)
+    if len(text) == 0:
+        messagebox.showinfo('Error', 'Enter Letters!')
+    else:
+        result_ = to_cyrillic(text)
+        result.config(text=result_)
+
+
+def to_latin_func():
+    text = label_entry.get()
+    if len(text) == 0:
+        messagebox.showinfo('Error', 'Enter Letters!')
+    else:
+        result_ = to_cyrillic(text)
+        result.config(text=result_)
 
 
 # Button
-to_cyrillic_btn = Button(window, pady=10, padx=40, text='To Cyrillic', command=to_cyrillic_app)
+to_cyrillic_btn = Radiobutton(window, pady=10, padx=40, text=cyrill, command=to_cyrillic_func)
 to_cyrillic_btn.grid(row=2, column=1)
-
+to_latin_btn = Radiobutton(window, pady=10, padx=10, text=latin, command=to_latin_func)
+to_latin_btn.grid(row=2, column=2)
 # Output
 label_output = Label(window, text='Output: ', padx=10, pady=10)
 label_output.grid(row=3, column=0)
--- TkinterProjects/YTDowloader/app.py
+++ TkinterProjects/YTDowloader/app.py
@@ -35,14 +35,14 @@ def download_video():
 # Input and Label
 label_url = Label(window, text='URL: ', padx=10, pady=10)
 label_url.grid(row=1, column=0)
-entry_url = Entry(window, width=20)
+entry_url = Entry(window, width=50)
 entry_url.grid(row=1, column=1, pady=10, padx=10)
 
 # Buttons
 down_btn = Button(window, padx=10, pady=10, text='Download', command=download_video)
-down_btn.grid(row=2, column=2)
+down_btn.grid(row=2, column=1)
 
 info_button = Button(window, padx=10, pady=10, text='Video Info', command=show_info)
-info_button.grid(row=2, column=1)
+info_button.grid(row=3, column=1)
 if __name__ == '__main__':
     window.mainloop()
